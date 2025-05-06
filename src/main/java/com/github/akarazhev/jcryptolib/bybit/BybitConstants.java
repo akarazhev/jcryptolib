a185cd6efb65b1bf28cee638a8ff387aa9ab25e8
Andrey Karazhev <andrey.karazhev@specific-group.com>
2025-05-06 17:43:30 +0300

    Added bybit constants

---
+++ src/main/java/com/github/akarazhev/jcryptolib/bybit/BybitConstants.java
@@ -0,0 +1,9 @@
+package com.github.akarazhev.jcryptolib.bybit;
+
+public final class BybitConstants {
+    private BybitConstants() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static final String TOPIC_FIELD = "topic";
+}
