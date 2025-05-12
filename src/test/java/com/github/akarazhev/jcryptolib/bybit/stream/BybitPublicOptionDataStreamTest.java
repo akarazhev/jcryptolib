8b387631e9838e1a5b471357e7b4f86560461c6a
Andrey Karazhev <andrey.karazhev@specific-group.com>
2025-05-12 13:37:14 +0300

    Added stubs for tests.

---
+++ src/test/java/com/github/akarazhev/jcryptolib/bybit/stream/BybitPublicOptionDataStreamTest.java
@@ -0,0 +1,81 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Andrey Karazhev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.akarazhev.jcryptolib.bybit.stream;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class BybitPublicOptionDataStreamTest implements BybitPublicDataStreamTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BybitPublicOptionDataStreamTest.class);
+
+    @Test
+    @Override
+    public void shouldReceiveOrderBookDataStream() {
+
+    }
+
+    @Test
+    @Override
+    public void shouldReceiveTradeDataStream() {
+
+    }
+
+    @Test
+    @Override
+    public void shouldReceiveTickerDataStream() {
+
+    }
+
+    @Test
+    @Override
+    public void shouldReceiveKlineDataStream() {
+
+    }
+
+    @Test
+    @Override
+    public void shouldReceiveAllLiquidationDataStream() {
+
+    }
+
+    @Test
+    @Override
+    public void shouldReceiveLtKlineDataStream() {
+
+    }
+
+    @Test
+    @Override
+    public void shouldReceiveLtTickerDataStream() {
+
+    }
+
+    @Test
+    @Override
+    public void shouldReceiveLtNavDataStream() {
+
+    }
+}
