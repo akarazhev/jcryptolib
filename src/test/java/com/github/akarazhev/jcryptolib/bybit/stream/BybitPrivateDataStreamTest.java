f4c0a29a9646cc1081cfb87c2814009587e1f50d
Andrey Karazhev <andrey.karazhev@specific-group.com>
2025-06-03 14:41:30 +0300

    Implement the basic stub for the private data stream.

---
+++ src/test/java/com/github/akarazhev/jcryptolib/bybit/stream/BybitPrivateDataStreamTest.java
@@ -0,0 +1,80 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Andrey Karazhev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.akarazhev.jcryptolib.bybit.stream;
+
+import com.github.akarazhev.jcryptolib.bybit.BybitConstants;
+import com.github.akarazhev.jcryptolib.util.TestUtils;
+import io.reactivex.rxjava3.core.BackpressureStrategy;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.subscribers.TestSubscriber;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.net.http.HttpClient;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.github.akarazhev.jcryptolib.bybit.BybitConfig.getApiKey;
+import static com.github.akarazhev.jcryptolib.bybit.BybitConfig.getApiSecret;
+import static com.github.akarazhev.jcryptolib.bybit.BybitConfig.getPrivateTestnet;
+import static com.github.akarazhev.jcryptolib.bybit.BybitTestConfig.getPrivateOrder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+final class BybitPrivateDataStreamTest {
+    private static HttpClient client;
+
+    @BeforeAll
+    static void setup() {
+        client = HttpClient.newHttpClient();
+    }
+
+    @AfterAll
+    static void cleanup() {
+        client.close();
+    }
+
+    @Test
+    public void shouldReceiveOrderDataStream() {
+        final var stream = BybitDataStream.create(client, getApiKey(), getApiSecret(), getPrivateTestnet(), getPrivateOrder());
+        final var testSubscriber = new TestSubscriber<Map<String, Object>>();
+        Flowable.create(stream, BackpressureStrategy.BUFFER).subscribe(testSubscriber);
+        assertFalse(TestUtils.await(testSubscriber, 3, TimeUnit.SECONDS), "Should not receive any messages");
+
+        testSubscriber.assertNoErrors();
+        assertFalse(testSubscriber.values().isEmpty(), "Should receive at least one message");
+
+        testSubscriber.cancel();
+        TestUtils.sleep(1000);
+        final var countAfterCancel = testSubscriber.values().size();
+        TestUtils.sleep(1000);
+
+        assertEquals(countAfterCancel, testSubscriber.values().size(), "No new messages after cancel");
+        for (final var value : testSubscriber.values()) {
+            assertEquals(getPrivateOrder()[0], value.get(BybitConstants.TOPIC_FIELD));
+        }
+    }
+}
