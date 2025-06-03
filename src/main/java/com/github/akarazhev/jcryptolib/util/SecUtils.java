447987756708f4c0d42fdebb6f28b65ad0582196
Andrey Karazhev <andrey.karazhev@specific-group.com>
2025-06-03 11:37:35 +0300

    Reformat utils.

--- src/main/java/com/github/akarazhev/jcryptolib/util/SecUtils.java
+++ src/main/java/com/github/akarazhev/jcryptolib/util/SecUtils.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Andrey Karazhev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.github.akarazhev.jcryptolib.util;
 
 import javax.crypto.Mac;
@@ -21,7 +45,8 @@ public final class SecUtils {
      * @throws NoSuchAlgorithmException if the algorithm is not available
      * @throws InvalidKeyException      if the key is invalid
      */
-    public static String getSignature(final String secret, final long expires) throws NoSuchAlgorithmException, InvalidKeyException {
+    public static String getSignature(final String secret, final long expires)
+            throws NoSuchAlgorithmException, InvalidKeyException {
         final var val = "GET/realtime" + expires;
         final var secretKeySpec = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
         final var mac = Mac.getInstance("HmacSHA256");
